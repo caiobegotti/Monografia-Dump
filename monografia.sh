5ceb8b966dbe8b033a34748d617faecb666dba3f
caio1982 <caio1982@98c22b22-6958-4c52-8130-e24e0766ffce>
2011-11-21 17:33:19 +0000

    pela praticidade...

--- monografia.sh
+++ monografia.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+latex monografia.tex
+bibtex monografia
 sed '/@\(.*\){\(.*\),/!d;s//\2/' monografia.bib | while read c; do grep -q ${c} monografia.aux || echo cite ${c}; done
 
 exit 0
