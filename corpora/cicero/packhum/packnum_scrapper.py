28259384752b2288c5113265509238c21dbacc16
caio1982 <caio1982@98c22b22-6958-4c52-8130-e24e0766ffce>
2012-03-31 03:23:58 +0000

    faz download de um livro todo ja

--- corpora/cicero/packhum/packnum_scrapper.py
+++ corpora/cicero/packhum/packnum_scrapper.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import time
 from lxml import etree
 
 refs = []
@@ -8,12 +9,22 @@ urls = []
 
 browse = 'http://latin.packhum.org/author/474'
 page = etree.parse(browse, etree.HTMLParser(encoding='utf-8'))
-matches = page.xpath("//span[@class='wnam']//text()")
+#matches = page.xpath("//span[@class='wnam']//text()")
+
+matches = ['pro quinctio oratio']
 
 num = 1
 for entry in matches:
-    refs.append(('http://latin.packhum.org/dx/text/474/%s/0' % str(num), entry.lower()))
-    num += 1
+    refs.append(('http://latin.packhum.org/dx/text/474/%s/' % str(num), entry.lower()))
+    #num += 1
 
-for r in refs:
-    print r[1], r[0]
+for param in refs:
+    print 'processando: ' + param[1]
+    for x in range(0, 500):
+        section = param[0] + str(x)
+        page = etree.parse(section, etree.HTMLParser(encoding='utf-8'))
+        matches = page.xpath("//tr/td[1]//text()")
+        print ' '.join(matches)
+        if len(matches) == 0:
+            break
+        time.sleep(5)
