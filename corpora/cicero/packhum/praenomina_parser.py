c67462663e417de421101ba128c6470f6b4b5dd3
caio1982 <caio1982@98c22b22-6958-4c52-8130-e24e0766ffce>
2012-04-08 03:03:20 +0000

    adicionando parser de prenomes proprioes

---
+++ corpora/cicero/packhum/praenomina_parser.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# caio begotti <caio1982@gmail.com>
+# this is under public domain
+
+# reference: http://en.wiktionary.org/wiki/Appendix:Roman_praenomina
+
+import codecs
+import glob
+import re
+
+praenomina = []
+for file in glob.glob('./*.xml'):
+    content = codecs.open(file, "r", "utf8")
+    text = content.read()
+    regex = re.compile("[A-Z]'?\w{0,4}\. [A-Z]{0,}\w{0,}")
+    for entry in regex.findall(text):
+        praenomina.append(entry)
+
+praenomina = sorted(set(praenomina))
+for entry in praenomina:
+    print entry.lower()
+
+print len(praenomina)
