8dcc94e158d958b3c87841c9350aaf1c9db07929
caio1982 <caio1982@98c22b22-6958-4c52-8130-e24e0766ffce>
2012-04-15 18:13:58 +0000

    versao de um esqueleto menos inutil...

--- corpora/cicero/packhum/concordances.py
+++ corpora/cicero/packhum/concordances.py
@@ -4,5 +4,23 @@
 # this is under public domain
 
 import glob
+import optparse
 
 from nltk.tokenize import word_tokenize
+
+def search():
+    parser = optparse.OptionParser("Usage: %prog [options]")
+    parser.add_option("-s", "--search", type="string", dest="term",
+                      help="search concordances for a word")
+    parser.add_option("-f", "--fake", action="store_true", dest="fake",
+                      default=False, help="considers non-ciceronian texts")
+ 
+    (options, args) = parser.parse_args()
+    if options.term is None:
+        parser.print_help()
+        exit(-1)
+    else:
+        print options
+
+if __name__ == "__main__":
+    search()
