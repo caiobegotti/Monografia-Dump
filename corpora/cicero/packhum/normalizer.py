a5ead9b4a356188d8128271df7e1e48d04b3d5ed
caio1982 <caio1982@98c22b22-6958-4c52-8130-e24e0766ffce>
2012-04-15 01:46:28 +0000

    normalizador dos textos, converter j pra i, v pra u etc

---
+++ corpora/cicero/packhum/normalizer.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# caio begotti <caio1982@gmail.com>
+# this is under public domain
+
+import codecs
+import glob
+import re
+
+def normalizer():
+    for loop in glob.glob('ready/*.txt'):
+        text = ''
+        with file(loop, 'r') as content:
+            text = content.read().lower()
+
+        # fixes the linebreaking of corpora
+        text = text.replace("- ", "")
+
+        # fixes unused letters for their real latin ones
+        text = text.replace("v","u")
+        text = text.replace("j","i")
+       
+        with file(loop, 'w') as content:
+            content.write(text)
+
+if __name__ == "__main__":
+    normalizer()
