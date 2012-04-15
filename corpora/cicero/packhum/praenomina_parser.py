3fe14fb447dd19b4945a4e902d7ba5cd4c9b219d
caio1982 <caio1982@98c22b22-6958-4c52-8130-e24e0766ffce>
2012-04-15 02:35:37 +0000

    substituindo open por with file que eh mais seguro e arrumando regex de nomes pra nao ter falsos positivos

--- corpora/cicero/packhum/praenomina_parser.py
+++ corpora/cicero/packhum/praenomina_parser.py
@@ -11,33 +11,33 @@ import glob
 import re
 
 def parser():
-    regex = re.compile("[A-Z]'?\w{0,4}\. [A-Z]{0,}\w{0,} [A-Z]{0,}\w{0,}")
+    regex = re.compile("(?:A|Ap|D|C|Cn|K|L|M|Mam|N|O|P|Q|Qu|S|Sp|Ser|Sex|Sec|Seq|Sept|T|Ti|Tit|Vel|Vo)'?\. [A-Z]{0,}\w{0,} [A-Z]{0,}\w{0,}")
     praenomina = []
     
-    for file in glob.glob('ready/orator-*.txt'):
-        content = codecs.open(file, "r", "utf8")
-        text = content.read()
-        for entry in regex.findall(text):
-            praenomina.append(entry)
+    for loop in glob.glob('ready/*.txt'):
+        with file(loop, 'r') as content:
+            text = content.read()
+            for entry in regex.findall(text):
+                praenomina.append(entry)
     
     return sorted(set(praenomina))
 
 def replacer():
     list = parser()
     regex = re.compile("^(.*)\. ")
-    for file in glob.glob('ready/orator-5.txt'):
-        content = codecs.open(file, "w", "utf8")
-        text = content.read()
-        replaced = ''
-        for entry in list:
-            r = regex.search(entry)
-            match = r.group(1)
-            name = re.sub('^' + match, '(' + match + ')', entry)
-            name = name.replace('.', '')
-            print 'era -> "' + entry + '" e vai virar -> "' + name + '"' + ' em ' + file
-            replaced = re.sub(entry, name, text)
-        content.write(replaced)
-        content.close()
+    for loop in glob.glob('ready/*.txt'):
+        with file(loop, 'r') as content:
+            text = content.read()
+            replaced = ''
+            for entry in list:
+                r = regex.search(entry)
+                match = r.group(1)
+                name = re.sub('^' + match, '(' + match + ')', entry)
+                name = name.replace('.', '')
+                replaced = re.sub(entry, name, text)
+                with file(loop, 'w') as content:
+                    content.write(replaced)
 
 if __name__ == "__main__":
-    replacer()
+    for x in parser():
+        print x
