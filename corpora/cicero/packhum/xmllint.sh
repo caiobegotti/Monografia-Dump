8c1e9e81e53ac1e63ab5f9447ca822db10cf9675
caio1982 <caio1982@98c22b22-6958-4c52-8130-e24e0766ffce>
2012-04-22 04:26:37 +0000

    encoding utf-8 explicito

--- corpora/cicero/packhum/xmllint.sh
+++ corpora/cicero/packhum/xmllint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-for file in $(ls -1 *.xml); do xmllint --format ${file} > /tmp/${0} && mv -v /tmp/${0} ${file}; done
+for file in $(ls -1 *.xml); do xmllint --encode utf-8 --format ${file} > /tmp/${0} && mv -v /tmp/${0} ${file}; done
 
 exit 0
