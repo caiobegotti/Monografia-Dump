fa36a7c32fc34f033ae01c93a0106bc8e349238c
caio1982 <caio1982@98c22b22-6958-4c52-8130-e24e0766ffce>
2012-05-07 00:44:35 +0000

    ja aproximando do resultado final, imprimindo diretamente pra organizar no latex as listas de sugestoes de lexico por corpus

--- corpora/cicero/packhum/corpus_stats.py
+++ corpora/cicero/packhum/corpus_stats.py
@@ -15,21 +15,16 @@ from nltk import FreqDist
 from nltk import Text
 
 for corpus in cicero.fileids():
-    print 'Corpus: ' + corpus
-
     reader = CategorizedXMLCorpusReader(cicero.root,
                                         cicero.abspaths(),
                                         cat_file='categories.txt')
-    data = Text(reader.words([corpus]))
-    dist = FreqDist(data)
-   
-    print 'Data lenght: ' + str(len(data))
-    print 'Distribution of: ' + str(len(dist))
-    print '\nCOUNT\tP(%)\tTERM'
+    try:
+        dist = FreqDist(Text(punctless(stopless(reader.words([corpus])))))
+    except UnicodeEncodeError as e:
+        print '\nXXX ERROR XXX\n' + str(e)
 
-    total = len(dist.items())
-    for item in dist.items():
-        if len(item[0]) >= 1:
-            percentage = dist.freq(item[0]) * 100
-            percentage = '{0:.3}'.format(percentage)
-            print '%d\t%s\t%s' % (item[1], percentage + '%', item[0])
+    rank = []
+    for item in dist.items()[:100]:
+        if len(item[0]) >= 2:
+            rank.append(item[0])
+    print corpus + ': ' + ', '.join(rank)
