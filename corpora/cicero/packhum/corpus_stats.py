5b3ec53f04b3726678f7b0e76391345a72c8e821
caio1982 <caio1982@98c22b22-6958-4c52-8130-e24e0766ffce>
2012-05-15 00:10:09 +0000

    nova versao que limpa repeticoes e soh cospe os lemmas pra limpar no final manualmente

--- corpora/cicero/packhum/corpus_stats.py
+++ corpora/cicero/packhum/corpus_stats.py
@@ -15,7 +15,26 @@ from nltk import Text
 # experimental
 from latin_lemmatizer import lemmatize
 
-for corpus in cicero.fileids():
+list = [
+'cicero_de_republica.xml',
+'cicero_brutus.xml',
+'cicero_de_divinatione.xml',
+'cicero_rhetorica_ad_herennium_sp.xml',
+'cicero_de_inventione.xml',
+'cicero_de_natura_deorum.xml',
+'cicero_de_officiis.xml',
+'cicero_tusculanae_disputationes.xml',
+'cicero_de_finibus.xml',
+'cicero_philippicae.xml',
+'cicero_de_oratore.xml',
+'cicero_in_verrem.xml',
+'cicero_epistulae_ad_familiares.xml',
+'cicero_epistulae_ad_atticum.xml',
+'cicero_tusculanae_disputationes.xml'
+]
+
+total = []
+for corpus in list:
     reader = CategorizedXMLCorpusReader(cicero.root,
                                         cicero.abspaths(),
                                         cat_file='categories.txt')
@@ -25,11 +44,14 @@ for corpus in cicero.fileids():
         print str(e)
         break
 
-    rank = []
+    definitions = {}
     stat = reader.words([corpus])
     if len(stat) >= 25000:
-        for item in dist.items()[:100]:
+        for item in dist.items()[:150]:
             if len(item[0]) >= 2:
-                lemma = ' (%s)' % lemmatize(item[0])
-                rank.append(item[0] + lemma)
-        print '\n%s: %s' % (corpus, ', '.join(rank))
+                lemma = lemmatize(item[0])
+                if lemma is not None:
+                    if lemma not in total:
+                        definitions[item[0]] = lemma
+                        total.append(lemma)
+    print corpus + ': ' + ', '.join(sorted(definitions.values()))
