8ffbe5d20389ead70cfc8f9fb16260df37f8411f
caio1982 <caio1982@98c22b22-6958-4c52-8130-e24e0766ffce>
2012-06-05 21:47:20 +0000

    leftover

--- corpora/cicero/packhum/corpus_stats.py
+++ corpora/cicero/packhum/corpus_stats.py
@@ -18,23 +18,6 @@ from latin_lemmatizer import lemmatize
 # fancy dictionary
 from collections import defaultdict
 
-list = [
-'cicero_de_republica.xml',
-'cicero_brutus.xml',
-'cicero_de_divinatione.xml',
-'cicero_rhetorica_ad_herennium_sp.xml',
-'cicero_de_inventione.xml',
-#'cicero_de_natura_deorum.xml',
-#'cicero_de_officiis.xml',
-#'cicero_de_finibus.xml',
-#'cicero_philippicae.xml',
-#'cicero_de_oratore.xml',
-#'cicero_in_verrem.xml',
-#'cicero_epistulae_ad_familiares.xml',
-#'cicero_epistulae_ad_atticum.xml',
-#'cicero_tusculanae_disputationes.xml'
-]
-
 total = defaultdict(int)
 for corpus in cicero.fileids():
     print corpus
