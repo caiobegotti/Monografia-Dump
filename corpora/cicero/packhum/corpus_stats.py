bed6686b83a553483313d64663a0df4c962098b9
caio1982 <caio1982@98c22b22-6958-4c52-8130-e24e0766ffce>
2012-06-05 21:46:28 +0000

    versao do corpus.txt de 1000 termos de todos os textos!

--- corpora/cicero/packhum/corpus_stats.py
+++ corpora/cicero/packhum/corpus_stats.py
@@ -19,24 +19,25 @@ from latin_lemmatizer import lemmatize
 from collections import defaultdict
 
 list = [
-#'cicero_de_republica.xml',
-#'cicero_brutus.xml',
-#'cicero_de_divinatione.xml',
-#'cicero_rhetorica_ad_herennium_sp.xml',
-#'cicero_de_inventione.xml',
+'cicero_de_republica.xml',
+'cicero_brutus.xml',
+'cicero_de_divinatione.xml',
+'cicero_rhetorica_ad_herennium_sp.xml',
+'cicero_de_inventione.xml',
 #'cicero_de_natura_deorum.xml',
 #'cicero_de_officiis.xml',
 #'cicero_de_finibus.xml',
 #'cicero_philippicae.xml',
-'cicero_de_oratore.xml',
-'cicero_in_verrem.xml',
-'cicero_epistulae_ad_familiares.xml',
-'cicero_epistulae_ad_atticum.xml',
-'cicero_tusculanae_disputationes.xml'
+#'cicero_de_oratore.xml',
+#'cicero_in_verrem.xml',
+#'cicero_epistulae_ad_familiares.xml',
+#'cicero_epistulae_ad_atticum.xml',
+#'cicero_tusculanae_disputationes.xml'
 ]
 
 total = defaultdict(int)
-for corpus in list:
+for corpus in cicero.fileids():
+    print corpus
     reader = CategorizedXMLCorpusReader(cicero.root,
                                         cicero.abspaths(),
                                         cat_file='categories.txt')
